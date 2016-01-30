d8ab3138d25d15c755b42d6a2923e98843fe480a
June Andrea Celo <junec@junec.local>
2016-01-30 22:46:40 +0800

    Changed music files heirarchy Added intro dance audio and fail audio Created AudioController Prefab Added .DS_Store to .gitignore file

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -2,48 +2,72 @@
 using System.Collections;
 
 [RequireComponent(typeof(AudioSource))]
-public class MusicController : MonoBehaviour {
+public class AudioController : MonoBehaviour {
 
 	private AudioSource currentAudio = null;
 
-	public AudioSource bgAudio;
+	public AudioSource menuAudio;
 	public AudioSource danceAudio;
 	public AudioSource highScoreAudio;
 	public AudioSource aboutAudio;
+	public AudioSource introAudio;
+	public AudioSource failAudio;
 
 	// Use this for initialization
 	void Start() {
 		// set audio settings
 		aboutAudio.loop = true;
 		aboutAudio.playOnAwake = false;
-		bgAudio.loop = true;
-		bgAudio.playOnAwake = false;
+
+		menuAudio.loop = true;
+		menuAudio.playOnAwake = false;
+
 		danceAudio.loop = true;
 		danceAudio.playOnAwake = false;
+
 		highScoreAudio.loop = true;
 		highScoreAudio.playOnAwake = false;
 
-		// start background music
-		StartBgMusic();
+		introAudio.loop = false;
+		introAudio.playOnAwake = false;
+
+		failAudio.loop = false;
+		failAudio.playOnAwake = false;
+
+		StartMenuMusic();
 	}
 
 	// Starts the about music
 	public void StartAboutMusic() {
 		clean();
+		introAudio.Play();
 		aboutAudio.Play();
 		currentAudio = aboutAudio;
 	}
 
 	// Starts the background/main menu music 
-	public void StartBgMusic() {
+	public void StartMenuMusic() {
 		clean();
-		bgAudio.Play();
-		currentAudio = bgAudio;
+		menuAudio.Play();
+		currentAudio = menuAudio;
 	}
 
 	// Starts the dance/play music
 	public void StartDanceMusic() {
-		clean();
+		menuAudio.volume = (float)0.5;
+		StartCoroutine(playDanceClip());
+	}
+
+	// Coroutine for StartDanceMusic
+	// Play intro music, then follow with dance music
+	IEnumerator playDanceClip(){
+		AudioClip danceClip = danceAudio.clip;
+		AudioClip introClip = introAudio.clip;
+
+		danceAudio.clip = introClip;
+		danceAudio.Play();
+		yield return new WaitForSeconds(danceAudio.clip.length);
+		danceAudio.clip = danceClip;
 		danceAudio.Play();
 		currentAudio = danceAudio;
 	}
@@ -55,12 +79,23 @@ public class MusicController : MonoBehaviour {
 		currentAudio = highScoreAudio;
 	}
 
+	// Starts fail music
+	public void StartFailMusic(){
+		clean();
+		failAudio.Play();
+		currentAudio = failAudio;
+	}
+
 	// Used to make sure that any existing/current AudioClip
 	// is stopped before playing a new AudioClip.
 	private void clean(){
 		if (currentAudio != null) {
 			currentAudio.Stop();
-		}	
+		}
+
+		if (menuAudio.isPlaying) {
+			menuAudio.Stop ();
+		}
 	}
 		
 	// Update is called once per frame
