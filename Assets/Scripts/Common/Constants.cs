b1a1c12cc20b6e70dc5f24c087d8ce9d18882ba2
Renemari Padillo <padillo.renemari@gmail.com>
2016-01-30 13:39:22 +0800

    [CHORE] Added scenes, scripts, and inital UI

---
+++ Assets/Scripts/Common/Constants.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// All unchanging values that can be used through all the scripts are entered here.
+/// </summary>
+public static class Constants {
+
+	// Playerprefs
+	public static string HIGH_SCORE = "TR_HIGH_SCORE";
+
+}
